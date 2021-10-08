e6218e77f50270ed7202f10ed92d0f66b699c902
Ralucadancu <91536654+Ralucadancu@users.noreply.github.com>
2021-10-08 18:02:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,40 @@ def test_is_superprime():
     else:
         print("Nu este superprim ")
 
+
+
+
+"Problema numarul 3"
+
+def is_prime(n):
+
+    if (n==0 or n==1):
+      return False
+    for d in range (2,n//2):
+        if (n%d==0):
+          return False
+    return True
+
+def get_goldbach(n):
+    ok=True
+    for i in range (2,n//2+1):
+        p1=i
+        p2=n-i
+        if is_prime(p1) and is_prime(p2):
+            ok=False
+            print (p1,p2)
+            break
+        if ok==True:
+           print("Nu are proprietatea")
+           
+
+def test_get_goldbach():
+    n=int(input("3) n="))
+    get_goldbach(n)
+                       
+
 test_get_largest_prime_below()
 test_is_superprime()
-        
-          
+test_get_goldbach()
+
+
